ed87f20bfaef27a4333b4f084e95fb5ccfc4b5f2
namara-pipeline <git@namara-pipeline.com>
2024-10-27 18:48:18 +0900

    to README.md

--- src/main.c
+++ src/main.c
@@ -1,22 +1,3 @@
-// VSCode設定
-//   https://creating-homepage.com/archives/8653
-//     Editor: Insert Spaces
-//     Editor: Tab Size
-//   https://atmarkit.itmedia.co.jp/ait/articles/1809/14/news025.html
-//     files:eol
-//   https://www.javadrive.jp/vscode/setting/index4.html
-//     files:Encoding
-//
-// https://www.javadrive.jp/cstart/install/index6.html
-//    MinGW-W64-builds  https://www.mingw-w64.org/downloads/#mingw-w64-builds
-//        https://github.com/niXman/mingw-builds-binaries/releases
-//            x86_64-14.2.0-release-posix-seh-ucrt-rt_v12-rev0.7z
-//            mv mingw64 C:\mingw64
-//            setx PATH "%PATH%;C:\mingw64\bin"
-//
-// https://qiita.com/OcoToOo/items/f1d0a125327f5659ad52
-//    c_cpp_properties.json
-
 #include <stdio.h>
 #include "../private/main.h"
 
